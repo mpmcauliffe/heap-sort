cb26be8b1016da82685aaec4d0d0f58ac7d8d9a7
mpmcauliffe <mcauliffe99@gmail.com>
2019-05-13 20:40:11 -0600

    sort complete

--- src/algorithms/Heap.java
+++ src/algorithms/Heap.java
@@ -42,6 +42,19 @@ public class Heap {
         return heap[0];
     }
 
+    public void sort() {
+        int lastHeapIndex = size - 1;
+
+        for (int i = 0; i < lastHeapIndex; i++) {
+            int tmp = heap[0];
+            heap[0] = heap[lastHeapIndex - i];
+            heap[lastHeapIndex - i] = tmp;
+
+            fixHeapBelow(0, lastHeapIndex - i - 1);
+        }
+    }
+
+
     private void fixHeapAbove(int index) {
         int newValue = heap[index];
 
--- src/algorithms/Main.java
+++ src/algorithms/Main.java
@@ -3,6 +3,59 @@ package algorithms;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.print("\n\n");
+
+    /**
+     *  TESTING
+     **/
+        // instantiate
+        Heap heap = new Heap(10);
+
+        // insert
+        heap.insert(80);
+        heap.insert(75);
+        heap.insert(60);
+        heap.insert(68);
+        heap.insert(55);
+        heap.insert(40);
+        heap.insert(52);
+        heap.insert(67);
+
+        // print
+        heap.printHeap();
+
+        // delete
+        heap.delete(1);
+        heap.printHeap();
+
+        heap.delete(5);
+        heap.printHeap();
+
+        heap.delete(0);
+        heap.printHeap();
+
+        // peek
+        System.out.println(heap.peek());
+        System.out.print("\n");
+
+        /** SORT **/
+        System.out.println("SORTING");
+        heap.sort();
+        heap.printHeap();
+
+        System.out.println("Inserting 76 23 90 11 1");
+        heap.insert(76);
+        heap.insert(23);
+        heap.insert(90);
+        heap.insert(11);
+        heap.insert(1);
+        heap.printHeap();
+
+        System.out.println("SORTING");
+        heap.sort();
+        heap.printHeap();
+
+
+        System.out.print("\n\n");
     }
 }
